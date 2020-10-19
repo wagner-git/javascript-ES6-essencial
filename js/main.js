68fe88a3873a5f7fe7ed40714cf7493e518d5fb0
wagner <wagner_info300@yahoo.com.br>
2020-10-19 18:28:21 -0300

    Nova aula/

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*const user = {
+    name: "Helena luisa",
+    lastName: "Ferreira"
+};
+
+function getUserWithFullName(user){
+    return {
+        ...user,
+        fullName: user.name + " " + user.lastName
+    }
+}
+
+const userWithFullName = getUserWithFullName(user);
+console.log(userWithFullName);*/
